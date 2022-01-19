69770582c9e06a5bb1211549c32dd54c7fd30b88
ssathyaa <98035068+ssathyaa@users.noreply.github.com>
2022-01-19 19:57:26 +0530

    day 1 assignment submitted

---
+++ python_assignment1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+list1=[1,2,3]
+tuple1=(4,5,6)
+set1={7,8,9}
+dict1={1:"one",2:"two"}
+print(list1 + list(tuple1) + list(set1)) ,print(dict1)
+
+
+# In[ ]:
+
+
+
+
